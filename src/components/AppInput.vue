5720521e5ebef4681a4292b22abe8bd585787d04
AKM04 <abdulghanim70@gmail.com>
2025-08-25 09:16:51 +0300

    feat: input component and used

---
+++ src/components/AppInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex w-full flex-row items-center gap-2">
+    <p class="text-teal-800 text-xl">
+      {{label}}:
+    </p>
+  <input :type="type" v-model="modalValue" class="border border-teal-800 rounded-xl p-2" :placeholder="placeholder"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  placeholder: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: 'text'
+  },
+
+})
+const modalValue = defineModel<string>()
+</script>
+
+
--- src/pages/students.vue
+++ src/pages/students.vue
@@ -8,9 +8,12 @@
         students in the our organization are high level students!
       </p>
 
-      <input type="text" class="border border-teal-800 rounded-xl p-2" placeholder="Search Students" v-model="searchTerm"></input>
-
-      <AppTable :data="filteredStudents" :headers="headers"/>
+      <div class="mt-12 flex flex-col items-center gap-6">
+        <div class="w-full">
+          <AppInput label="Search for a Student" type="text" placeholder="Search Students" v-model="searchTerm"/>
+        </div>
+        <AppTable :data="filteredStudents" :headers="headers"/>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +23,7 @@ import {onMounted, ref, computed} from "vue";
 import {useStudentsStore} from "../stores/students";
 import {search} from "../utils";
 import AppTable from "../components/AppTable.vue";
+import AppInput from "../components/AppInput.vue";
 const searchTerm = ref('')
 const filteredStudents = computed(() => search(studentsStore.students, searchTerm.value));
 const headers = ['ID','Name', 'Age', 'Gender', 'Stage', 'Absents', 'Special?', 'Subjects']
